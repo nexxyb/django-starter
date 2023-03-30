17b8fae91ef4e5b64eaf77379608eea82bdeeccb
Wasiu <nexxyb@gmail.com>
2023-03-30 09:51:53 +0100

    raw tags added to templates

--- {{cookiecutter.project_slug}}/main/core/settings.py
+++ {{cookiecutter.project_slug}}/main/core/settings.py
@@ -9,9 +9,6 @@ from sentry_sdk.integrations.django import DjangoIntegration
 BASE_DIR = Path(__file__).parent.parent
 CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Take environment variables from .env file
-#environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY= os.environ.get('SECRET_KEY')
 
---
+++ {{cookiecutter.project_slug}}/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# create virtual environment
+cd main
+
+python3 -m venv venv
+source venv/bin/activate
+
+# install packages from requirements.txt
+
+pip install -r requirements.txt
+
+
+# run Django migrations
+python manage.py migrate
+python manage.py runserver
