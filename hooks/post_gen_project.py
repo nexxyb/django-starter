3091b5e510b065ee16686d2b2d45bcc4aeb7407c
Wasiu <nexxyb@gmail.com>
2023-03-30 10:09:43 +0100

    hook added

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,5 +2,9 @@
 
 import os
 
+TERMINATOR = "\x1b[0m"
+SUCCESS = "\x1b[1;32m [SUCCESS]: "
+
 project_slug = os.path.basename(os.getcwd())
-print(f"Project {project_slug} started!")
+print(SUCCESS + f"Project {project_slug} initialized, keep up the good work!" + TERMINATOR)
+
